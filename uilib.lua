78130db4565611994680ca635985eb1fdff522af
rocoScripter <rocogamingbackup@gmail.com>
2026-05-17 17:18:36 +1000

    Update uilib.lua

--- uilib.lua
+++ uilib.lua
@@ -9,7 +9,7 @@ local RenderStepped = RunService.RenderStepped;
 local LocalPlayer = Players.LocalPlayer;
 local Mouse = LocalPlayer:GetMouse();
 
-local ProtectGui = gethui() or (syn and syn.protect_gui) or (function() end);
+local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
 local ScreenGui = Instance.new('ScreenGui');
 ProtectGui(ScreenGui);
